ea6468a3f611fd50d11ef9ba4d46c2c66886583a
sumaya-kabir <sumayakabir4547@gmail.com>
2022-10-26 18:26:01 +0600

    course detail api setup

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
     res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const course = courses.find(ht => ht.id === id );
+    res.send(course);
 })
 
 app.listen(port, () => {
